096c039604f9fb93f5c2c085d346ea30ac64c48e
felixdapony2 <85595042+felixdapony2@users.noreply.github.com>
2021-06-09 16:53:50 -0400

    Added Heroku Dependicnies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5bd12ffbf25686f03be2a3eac3786977d768ac1d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5bd12ffbf25686f03be2a3eac3786977d768ac1d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
