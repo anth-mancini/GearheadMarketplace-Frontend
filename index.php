f0d656a89f43a552e75f607f5bae6629d4f6bafa
felixdapony2 <85595042+felixdapony2@users.noreply.github.com>
2021-06-09 21:07:19 -0400

    trying get

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 
 ?>
 <html>
-<h1>stuff here</h1>
-<p>trying to push an update</p>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+    <h1>stuff here</h1>
+    <p>trying to push an update</p>
+<button id="someid">Click me!</button>
+</body>
 </html>
 
 <script>
+$("#someid").on("click", function(){
 
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://backend-gearheadmarketplace.herokuapp.com',
+        success: function(data){
+            console.log("clicked");
+            console.log(data);
+        }
+    })
+})
 </script>
