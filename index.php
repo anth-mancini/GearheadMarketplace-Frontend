05b1918e04331e1d932107594a4b1b41d1f41381
felixdapony2 <85595042+felixdapony2@users.noreply.github.com>
2021-07-01 18:06:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,20 +8,37 @@
 <body>
     <h1>stuff here</h1>
     <p>trying to push an update. checking</p>
-<button id="someid">Click me!</button>
+    <button id="someid">Click me!</button>
+    <button id="postid">Post data</button>
 </body>
 </html>
 
 <script>
-$("#someid").on("click", function(){
-    console.log("clicked");
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:8000',
-        // url: 'https://backend-gearheadmarketplace.herokuapp.com',
-        success: function(data){
-            console.log(data);
-        }
+    let myData = {email: "someemail@gmail.com", password: "123456"};
+    $("#someid").on("click", function(){
+        console.log("clicked");
+        $.ajax({
+            type: 'GET',
+            url: 'http://0.0.0.0:8000/users/?skip=0&limit=100',
+            // url: 'https://backend-gearheadmarketplace.herokuapp.com',
+            success: function(data){
+                console.log(data);
+            }
+        })
     })
-})
+
+    $("#postid").on("click", function(){
+        console.log(JSON.stringify(myData));
+        console.log("clicked");
+        $.ajax({
+            type: 'post',
+            url: 'http://0.0.0.0:8000/users/',
+            // url: 'https://backend-gearheadmarketplace.herokuapp.com',
+            data: JSON.stringify({"email":"someother@gmail.com", "password":"1234566"}),
+            dataType: "json",
+            success: function(data){
+                console.log(data);
+            }
+        })
+    })
 </script>
