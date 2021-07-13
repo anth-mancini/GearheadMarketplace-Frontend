8e53c1bbac6892fb09da33c9ea2ce9beacfc2d32
felixdapony2 <85595042+felixdapony2@users.noreply.github.com>
2021-07-12 22:37:26 -0400

    trying to avoid cors

--- testForms/test-CP-API.php
+++ testForms/test-CP-API.php
@@ -21,7 +21,7 @@
 <script>
     $("form#addOffer").submit(function (e) {
         e.preventDefault();
-        let mailedBy = "2004381"; //customer ID
+        let mailedBy = "0009715261"; //customer ID
         let weight = "1";
         let originPostalCode = "H2B1A0";
         let postalCode = "K1K4T3";
@@ -38,17 +38,17 @@
         </destination>
       </mailing-scenario>`;
         // console.log(text);
-        var parser = new DOMParser();
-        var xmlDoc = parser.parseFromString(text, "text/xml");
-        console.log(xmlDoc);
+        // var parser = new DOMParser();
+        // var xmlDoc = parser.parseFromString(text, "text/xml");
+        // console.log(xmlDoc);
         // let xhr = new XMLHttpRequest();
-        // xhr.open('GET', 'https://ct.soa-gw.canadapost.ca/rs/ship/price')
-        // // xhr.open('GET', 'https://soa-gw.canadapost.ca/rs/ship/price')
+        // xhr.open('POST', 'https://ct.soa-gw.canadapost.ca/rs/ship/price')
         // xhr.setRequestHeader('Accept', 'application/vnd.cpc.ship.rate-v4+xml');
         // xhr.setRequestHeader('Content-Type', 'application/vnd.cpc.ship.rate-v4+xml');
         // xhr.setRequestHeader('Authorization', 'Basic ' + btoa('ed81d9146e37dc58:f54d3247e07b39d463b2eb'));
-        // xhr.setRequestHeader('Accept-language', 'en-CA');
-        // xhr.send(text);
+        // // xhr.setRequestHeader('Accept-language', 'en-CA');
+        //
+        // xhr.send(xmlDoc);
         // // 4. This will be called after the response is received
         // xhr.onload = function() {
         //     if (xhr.status != 200) { // analyze HTTP status of the response
@@ -70,21 +70,52 @@
         // xhr.onerror = function() {
         //     console.log("Request failed");
         // }
-        fetch('https://ct.soa-gw.canadapost.ca/rs/ship/price', {
-            method: 'get',
-            data: xmlDoc,
-            headers: {
-                Accept: 'application/vnd.cpc.ship.rate-v4+xml',
-                ContentType: 'application/vnd.cpc.ship.rate-v4+xml',
-                Authorization: 'Basic ' + btoa('ed81d9146e37dc58:f54d3247e07b39d463b2eb'),
-                AcceptLanguage: 'en-CA'
-            },
-        })
-            .then(response => response.json()).then(data => {
-                console.log(data)
-        }).catch(error => {
-            console.log(error)
-        })
+
+        var url = "https://ct.soa-gw.canadapost.ca/rs/ship/price";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+
+        xhr.setRequestHeader("Accept", "application/vnd.cpc.ship.rate-v4+xml");
+        xhr.setRequestHeader("Content-Type", "application/vnd.cpc.ship.rate-v4+xml");
+        xhr.setRequestHeader("Authorization", "Basic ZWQ4MWQ5MTQ2ZTM3ZGM1ODpmNTRkMzI0N2UwN2IzOWQ0NjNiMmVi");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                console.log(xhr.status);
+                console.log(xhr.responseText);
+            }};
+
+        var data = `<mailing-scenario xmlns="http://www.canadapost.ca/ws/ship/rate-v4">
+<customer-number>0009715261</customer-number>
+<parcel-characteristics>
+<weight>1</weight>
+</parcel-characteristics>
+<origin-postal-code>K2B8J6</origin-postal-code>
+<destination>
+<domestic>
+<postal-code>J0E1X0</postal-code>
+</domestic>
+</destination>
+</mailing-scenario>`;
+
+        xhr.send(data);
+
+        // fetch('https://ct.soa-gw.canadapost.ca/rs/ship/price', {
+        //     method: 'get',
+        //     data: xmlDoc,
+        //     headers: {
+        //         Accept: 'application/vnd.cpc.ship.rate-v4+xml',
+        //         ContentType: 'application/vnd.cpc.ship.rate-v4+xml',
+        //         Authorization: 'Basic ' + btoa('ed81d9146e37dc58:f54d3247e07b39d463b2eb'),
+        //         AcceptLanguage: 'en-CA'
+        //     },
+        // })
+        //     .then(response => response.json()).then(data => {
+        //         console.log(data)
+        // }).catch(error => {
+        //     console.log(error)
+        // })
     });
     function renderOffering(data){
 
