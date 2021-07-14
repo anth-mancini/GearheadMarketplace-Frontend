9dbc4b6d441e808281818e7091c27600a8ee59ea
felixdapony2 <85595042+felixdapony2@users.noreply.github.com>
2021-07-14 19:43:37 -0400

    Successful API CP Integration

--- testForms/test-CP-API.php
+++ testForms/test-CP-API.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -21,103 +18,17 @@
 <script>
     $("form#addOffer").submit(function (e) {
         e.preventDefault();
-        let mailedBy = "0009715261"; //customer ID
-        let weight = "1";
-        let originPostalCode = "H2B1A0";
-        let postalCode = "K1K4T3";
-        var text=`<mailing-scenario xmlns="http://www.canadapost.ca/ws/ship/rate-v4">
-        <customer-number>${mailedBy}</customer-number>
-        <parcel-characteristics>
-        <weight>${weight}</weight>
-        </parcel-characteristics>
-        <origin-postal-code>${originPostalCode}</origin-postal-code>
-        <destination>
-          <domestic>
-            <postal-code>${postalCode}</postal-code>
-          </domestic>
-        </destination>
-      </mailing-scenario>`;
-        // console.log(text);
-        // var parser = new DOMParser();
-        // var xmlDoc = parser.parseFromString(text, "text/xml");
-        // console.log(xmlDoc);
-        // let xhr = new XMLHttpRequest();
-        // xhr.open('POST', 'https://ct.soa-gw.canadapost.ca/rs/ship/price')
-        // xhr.setRequestHeader('Accept', 'application/vnd.cpc.ship.rate-v4+xml');
-        // xhr.setRequestHeader('Content-Type', 'application/vnd.cpc.ship.rate-v4+xml');
-        // xhr.setRequestHeader('Authorization', 'Basic ' + btoa('ed81d9146e37dc58:f54d3247e07b39d463b2eb'));
-        // // xhr.setRequestHeader('Accept-language', 'en-CA');
-        //
-        // xhr.send(xmlDoc);
-        // // 4. This will be called after the response is received
-        // xhr.onload = function() {
-        //     if (xhr.status != 200) { // analyze HTTP status of the response
-        //         console.log(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-        //     } else { // show the result
-        //         console.log(`Done, got ${xhr.response.length} bytes`); // response is the server response
-        //     }
-        // };
-        //
-        // xhr.onprogress = function(event) {
-        //     if (event.lengthComputable) {
-        //         console.log(`Received ${event.loaded} of ${event.total} bytes`);
-        //     } else {
-        //         console.log(`Received ${event.loaded} bytes`); // no Content-Length
-        //     }
-        //
-        // };
-        //
-        // xhr.onerror = function() {
-        //     console.log("Request failed");
-        // }
-
-        var url = "https://ct.soa-gw.canadapost.ca/rs/ship/price";
-
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", url);
-
-        xhr.setRequestHeader("Accept", "application/vnd.cpc.ship.rate-v4+xml");
-        xhr.setRequestHeader("Content-Type", "application/vnd.cpc.ship.rate-v4+xml");
-        xhr.setRequestHeader("Authorization", "Basic ZWQ4MWQ5MTQ2ZTM3ZGM1ODpmNTRkMzI0N2UwN2IzOWQ0NjNiMmVi");
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                console.log(xhr.status);
-                console.log(xhr.responseText);
-            }};
-
-        var data = `<mailing-scenario xmlns="http://www.canadapost.ca/ws/ship/rate-v4">
-<customer-number>0009715261</customer-number>
-<parcel-characteristics>
-<weight>1</weight>
-</parcel-characteristics>
-<origin-postal-code>K2B8J6</origin-postal-code>
-<destination>
-<domestic>
-<postal-code>J0E1X0</postal-code>
-</domestic>
-</destination>
-</mailing-scenario>`;
-
-        xhr.send(data);
-
-        // fetch('https://ct.soa-gw.canadapost.ca/rs/ship/price', {
-        //     method: 'get',
-        //     data: xmlDoc,
-        //     headers: {
-        //         Accept: 'application/vnd.cpc.ship.rate-v4+xml',
-        //         ContentType: 'application/vnd.cpc.ship.rate-v4+xml',
-        //         Authorization: 'Basic ' + btoa('ed81d9146e37dc58:f54d3247e07b39d463b2eb'),
-        //         AcceptLanguage: 'en-CA'
-        //     },
-        // })
-        //     .then(response => response.json()).then(data => {
-        //         console.log(data)
-        // }).catch(error => {
-        //     console.log(error)
-        // })
-    });
-    function renderOffering(data){
-
-    }
+                console.log("clicked");
+                let data = {"customerID": "0009715261", "toZIP" : "K2B8J6", "fromZIP":"J0E1X0"};
+                var ajaxurl = 'form.php';
+                $.post(ajaxurl, data, function (response) {
+                    // Response div goes here.
+                    // var parser = new DOMParser();
+                    // var xmlDoc = parser.parseFromString(response, "text/xml");
+                    let json = JSON.stringify(JSON.parse(response));
+                    json.forEach(function (obj){
+                        console.log(obj);
+                    })
+                });
+            });
 </script>
