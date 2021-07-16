a5ad24af12ba6bd72c987570905b5124eeced9dd
felixdapony2 <85595042+felixdapony2@users.noreply.github.com>
2021-07-16 16:40:03 -0400

    Integrated splash page and added login/logout functionality to the header

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
         </div>
     </div>
 </div>
+<?php include_once('user_splash.php'); ?>
 </body>
 <?php include_once('footer.php'); ?>
 </html>
--- login.php
+++ login.php
@@ -1,43 +1,40 @@
 <?php
+if (!session_id()) @ session_start();
 
 ?>
-
 <html>
 <head>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="../js/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
-
+<?php include_once('header.php'); ?>
 <body>
-<div class="container">
+<div class="container-fluid" style="height: 100vh;
+      background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url('/assets/images/hightech.jpg');">
     <div class="d-flex justify-content-center h-100">
         <div class="card">
             <div class="card-header">
-                <center><h3>Login</h3></center>
-
+                <div style="text-align: center;"><h3>Login</h3></div>
             </div>
             <div class="card-body">
-                <form id="loginTry" method="post" action="user_splash.php">
+                <form id="loginTry" method="post" action="loginsplash.php">
                     <input type="hidden" id="admin" placeholder="false" name="admin" value="false">
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            <span class="input-group-text"><img src="assets/icons/user.svg"></span>
                         </div>
                         <input type="text" class="form-control" placeholder="email" name="email"/>
-
                     </div>
+                    </br>
                     <div class="input-group form-group">
                         <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            <span class="input-group-text"><img src="assets/icons/key.svg"></span>
                         </div>
                         <input type="password" class="form-control" placeholder="password" name="password"/>
                     </div>
+                    </br>
                     <div class="row align-items-center remember">
                         <input type="checkbox">Remember Me
                     </div>
@@ -58,26 +55,25 @@
     </div>
 </div>
 </body>
+<?php include_once('footer.php'); ?>
 </html>
 <script>
-     let backendURL = 'http://0.0.0.0:8000/';
-     var status;
-    //let backendURL = 'https://backend-gearheadmarketplace.herokuapp.com/';
+    // let backendURL = 'http://0.0.0.0:8000/';
+    var status;
+    let backendURL = 'https://backend-gearheadmarketplace.herokuapp.com/';
     // console.log(backendURL)
     $("form#loginTry").submit(function (e) {
         e.preventDefault();
-        let formData = new FormData(this)
-        var convertedData = {}
+        let formData = new FormData(this);
+        var convertedData = {};
         //building a compatible data object for our post request
         //{key: value, key: value}
-        for(let f of formData.entries()) {
+        for (let f of formData.entries()) {
             convertedData[f[0]] = f[1]
         }
-        // console.log(convertedData);
         $.ajax({
             type: 'post',
             url: backendURL + 'login/',
-            // url: 'https://backend-gearheadmarketplace.herokuapp.com',
             data: JSON.stringify(convertedData),
             dataType: "json",
             headers: {
@@ -86,10 +82,10 @@
             },
             success: function (data) {
                 //status = data;
-                if(data !="Username or password is wrong") {
+                if (data != "Username or password is wrong") {
                     console.log(data);
                     $('#admin').val(data);
-                    $('form#loginTry').unbind('submit').submit()
+                    $('form#loginTry').unbind('submit').submit();
                 }
             }
         })
