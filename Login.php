bdec861b86a50f7e44f36c72e02d045e11eb2298
mancinii5 <mancini5@uwindsor.ca>
2021-07-16 16:36:23 -0400

    1

---
+++ Login.php
@@ -0,0 +1,98 @@
+<?php
+
+?>
+<html>
+<head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="../js/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+
+<body>
+<div class="container">
+    <div class="d-flex justify-content-center h-100">
+        <div class="card">
+            <div class="card-header">
+                <center><h3>Login</h3></center>
+
+            </div>
+            <div class="card-body">
+                <form id="loginTry" method="post" action="user_splash.php">
+                    <input type="hidden" id="admin" placeholder="false" name="admin" value="false">
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="email" name="email"/>
+
+                    </div>
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        </div>
+                        <input type="password" class="form-control" placeholder="password" name="password"/>
+                    </div>
+                    <div class="row align-items-center remember">
+                        <input type="checkbox">Remember Me
+</div>
+                    <div class="form-group">
+
+                            <input type="submit" value="Login" class="btn float-right login_btn">
+
+                    </div>
+                </form>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-center links">
+Don't have an account?<a href="#">Sign Up</a>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <a href="#">Forgot your password?</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+<script>
+     let backendURL = 'http://0.0.0.0:8000/';
+     var status;
+    //let backendURL = 'https://backend-gearheadmarketplace.herokuapp.com/';
+    // console.log(backendURL)
+    $("form#loginTry").submit(function (e) {
+        e.preventDefault();
+        let formData = new FormData(this)
+        var convertedData = {}
+        //building a compatible data object for our post request
+        //{key: value, key: value}
+        for(let f of formData.entries()) {
+            convertedData[f[0]] = f[1]
+        }
+        // console.log(convertedData);
+        $.ajax({
+            type: 'post',
+            url: backendURL + 'login/',
+            // url: 'https://backend-gearheadmarketplace.herokuapp.com',
+            data: JSON.stringify(convertedData),
+            dataType: "json",
+            headers: {
+            'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            success: function (data) {
+            //status = data;
+            if(data !="Username or password is wrong") {
+                console.log(data);
+                $('#admin').val(data);
+                $('form#loginTry').unbind('submit').submit()
+                }
+        }
+        })
+    })
+    </script>
--- index.php
+++ index.php
@@ -1,106 +1,112 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title> Home </title>
-    <link rel="stylesheet" href="css/homepage.css">
+    <script src="js/bootstrap.bundle.js"></script>
     <link rel="stylesheet" href="css/bootstrap.css">
+
+    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 </head>
-
-<body>
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poppins" />
-<nav class="navbar fixed-top navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">GearHead MarketPlace    |</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">What We Do</a>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Login
-                    </a>
-                </li>
-
-            </ul>
-        </div>
-    </div>
-
-</nav>
-
-
-
+    <link rel="stylesheet" href="css/header.css">
 <header>
-    <div class="wrapper">
-        <div class="logo">
-            <img src="assets/images/logo.JPG" alt="">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Gearhead Market Place</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            About Us
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="mission.php">Our Mission</a></li>
+                            <li><a class="dropdown-item" href="team.php">Our Team</a></li>
+                            <li><a class="dropdown-item" href="service.php">Our Services</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="media.php">Social Media</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">What we do</a>
+                    </li>
+                </ul>
+                <a href="login.php">
+                    <button class="btn btn-primary">Login</button>
+                </a>
+            </div>
         </div>
-
-    </div>
-
-    <div class="welcome-text">
-        <h1>Gear-Head Market Place</h1>
-        <a href="#">Contact Us</a>
-    </div>
+    </nav>
 </header>
-
-
-
-<!--FOOTER-->
+<body>
+<div class="px-4 py-5 my-6 text-center"
+     style="height: 100vh;
+      background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url('/assets/images/hightech.jpg');">
+    <img class="d-block mx-auto mb-4" src="assets/images/logo1.png" alt="" width="200" height="100">
+    <h1 class="display-5 fw-bold" style="color: white">Gearhead Marketplace</h1>
+    <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4" style="color: white">We are the best company in the world</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <a href="admin.php">
+                <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+            </a>
+            <button type="button" class="btn btn-danger btn-lg px-4">Secondary</button>
+        </div>
+    </div>
+</div>
+</body>
+<link rel="stylesheet" href="css/footer.css">
 <div class="footer">
-    <div class="footer-content">
-        <div class="footer-section about">
-            <h2 class="logo-text"><span>GearHead</span>MarketPlace</h2>
-            <p>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <h1>Gearhead Marketplace</h1>
                 GearHead MarketPlace is a user friendly interface capable of
                 connecting clients within North America to fufill their automotive
                 needs.
-            </p>
-        </div>
-        <div class="footer-section links">
-            <h2>Quick Links</h2>
-            <br>
-            <ul>
-                <a href="#">
-                    <li>About</li>
-                </a>
-                <a href="#">
-                    <li>What We Do</li>
-                </a>
-                <a href="#">
-                    <li>Contact Us</li>
-                </a>
-                <a href="#">
-                    <li>Login</li>
-                </a>
-
-            </ul>
-        </div>
-        <div class="footer-section contact">
-            <h2>Contact Us</h2>
-            <br>
-            <form action="index.php" method="post"
+            </div>
+            <div class="col-sm">
+                <h2>Quick Links</h2>
+                <br>
+                <ul>
+                    <a href="dashboard.php">
+                        <li>Login</li>
+                    </a>
+                    <a href="mission.php">
+                        <li>Mission</li>
+                    </a>
+                    <a href="service.php">
+                        <li>Services</li>
+                    </a>
+                    <a href="team.php">
+                        <li>Team</li>
+                    </a>
+                    <a href="media.php">
+                        <li>Social Media</li>
+                    </a>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <h2>Contact Us</h2>
+                <br>
+                <form action="index.php" method="post"
                 <input type="email" name="email" class="#" placeholder="Your email address..">
                 <textarea name="message" class="#" placeholder="Your message.."></textarea>
-            </form>
-
+                </form>
+            </div>
         </div>
     </div>
     <div class="footer-bottom">
         &copy; gearheadmarketplace.com | Designed by: Team 7
     </div>
-</div>
-
-</body>
-</html>
+</div></html>
