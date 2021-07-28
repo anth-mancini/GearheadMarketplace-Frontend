5808839b407b912e71a5038257e539d15eb1c3b7
mattmartin1 <48999374+mattmartin1@users.noreply.github.com>
2021-07-28 14:55:08 -0400

    faq done

--- faq.php
+++ faq.php
@@ -9,22 +9,48 @@
 <?php include_once('header.php'); ?>
 <body>
 <div class="px-4 py-5 my-6 text-center img-fluid" style="
-     background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url('/assets/images/IMG_4332.jpeg') fixed center no-repeat;
+     background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url('/assets/images/IMG_4332.jpeg') ;
      background-size: cover;
-     height: 100vh;">
+     ">
     <br>
     <br>
-    <h1 class="display-5 fw-bold" style="color: white">How to Use our Store / F.A.Q</h1>
-    <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4" style="color: white">ENTER USER GUIDE HERE</p>
+    <h1 class="display-5 fw-bold" style="color: white">How to Use our Store</h1>
+        <p class="lead mb-4" style="color: white">
+            First things first, signup for an account
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/signup.png" alt="" width = "300" height = "auto" >
+            Once you login you will find yourself on the browse listings page
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/splash.PNG" alt="" width="700" height="auto">
+            Click an offer to see more details
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/offer.PNG" alt="" width="500" height="auto">
+            Here you can find two tools. Contact the Seller via Email and Get a Shipping Quote
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/buttons.PNG" alt="" width="300" height="auto">
+            The contact button will open local mail client and pre-fill necessary information
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/mail.png" alt="" width="500" height="auto">
+            The shipping quote button will open the live-data set which pulls shipping prices from different companies
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/shipping.png" alt="" width="600" height="auto">
+            <br>
+            To create a listing click the account dropdown in the header and select "Create Listing". Fill out the form with the required info and submit
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/account.png" alt="" width="150" height="auto">
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/listing_new.PNG" alt="" width="300" height="auto">
+            The settings button will bring a user to their dashboard where they can edit their account info
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/account_info.PNG" alt="" width="600" height="auto">
+            My listings will open all listings made with your account. Here you can edit or remove them
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/mylistings.PNG" alt="" width="400" height="auto">
+            <img class="d-block mx-auto mb-4" src="assets/doc_images/edit.PNG" alt="" width="400" height="auto">
+
+    <h2 class="display-5 fw-bold" style="color: white">For any other questions, email help@gearhead.com</h2>
+
+
+
+        </p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <a href="login.php">
                 <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Login Now to Start Buying and Selling</button>
             </a>
         </div>
-    </div>
+
 </div>
 </body>
-<?php include_once('footer.php'); ?>
+
 
 </html>
