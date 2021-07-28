c814baaaeeedaf8c8010e6338815cd1699d8af98
mattmartin1 <48999374+mattmartin1@users.noreply.github.com>
2021-07-28 10:41:10 -0400

    logo update

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
      background: linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url('/assets/images/IMG_4332.jpeg') fixed center no-repeat;
      background-size: cover;
      height: 100vh;">
-    <img class="d-block mx-auto mb-4" src="assets/icons/logo_color.png" alt="" width="500" height="500">
+    <img class="d-block mx-auto mb-4" src="assets/icons/logo_white.png" alt="" width="500" height="500">
     <h1 class="display-5 fw-bold" style="color: white">Gearhead Marketplace</h1>
     <div class="col-lg-6 mx-auto">
         <p class="lead mb-4" style="color: white">Welcome to GearHead Marketplace, the #1 second-hand automotive store in North America</p>
